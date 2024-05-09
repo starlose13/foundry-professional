7038980add2b1be7c7292a5f04b223d86a6d9551
Pouya <jeopard13@yahoo.com>
2024-05-09 09:49:59 +0330

    anvil chain added as a mock contract for v3Aggregator

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -2,6 +2,7 @@
 
 pragma solidity 0.8.18;
 import {Script} from "forge-std/Script.sol";
+import {MockV3Aggregator} from "../test/Mocks/MockV3Aggregator.sol";
 
 contract HelperConfig is Script {
     NetworkConfig public activated;
@@ -15,6 +16,8 @@ contract HelperConfig is Script {
             activated = SepoliaChainLinkConfig();
         } else if (block.chainid == 2) {
             activated = EthereumChainLinkConfig();
+        } else {
+            activated = AnvilChainLinkConfig();
         }
     }
 
@@ -40,5 +43,16 @@ contract HelperConfig is Script {
             secondNeworkAddress: address(0)
         });
         return sepoliaConfig;
+    }
+
+    function AnvilChainLinkConfig() public returns (NetworkConfig memory) {
+        vm.startBroadcast();
+        MockV3Aggregator mockV3Aggregator = new MockV3Aggregator(8, 2000e8);
+        NetworkConfig memory anvilConfig = NetworkConfig({
+            activeChainAddress: address(mockV3Aggregator), // something else
+            secondNeworkAddress: address(0)
+        });
+        vm.stopBroadcast();
+        return anvilConfig;
     }
 }
